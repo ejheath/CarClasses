d78255b933afa1b904c5cbeb856ce7165d02aa13
Elijah Heath <ejheath8888@gmail.com>
2022-02-20 22:58:30 -0500

    classes with car types

---
+++ Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car()
+        {
+
+        }
+        public string Make { get; set; } //get = read set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+   
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2008;// dot notation
+
+
+            var honda = new Car()
+            {
+                Make = "Honda",
+                Model = "CRV",
+                Year = 2015
+            };
+
+            var ford = new Car( "Ford", "Gran Torino", 1972 );
+
+            var carList = new List<Car>() { myCar, honda, ford};
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+            }
+
+        }
+    }
+}
